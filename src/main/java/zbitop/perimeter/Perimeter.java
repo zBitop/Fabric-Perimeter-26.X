e7b0a0cd25f1ff97405a3b56743eda184b2fd96e
Bitop <bitofdez@gmail.com>
2026-07-22 02:31:08 -0400

    Initial Commit

---
+++ src/main/java/zbitop/perimeter/Perimeter.java
@@ -0,0 +1,22 @@
+package zbitop.perimeter;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.resources.Identifier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Perimeter implements ModInitializer {
+	public static final String MOD_ID = "perimeter";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+
+	public static Identifier id(String path) {
+		return Identifier.fromNamespaceAndPath(MOD_ID, path);
+	}
+}
---
+++ src/main/java/zbitop/perimeter/PerimeterDataGenerator.java
@@ -0,0 +1,11 @@
+package zbitop.perimeter;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PerimeterDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
